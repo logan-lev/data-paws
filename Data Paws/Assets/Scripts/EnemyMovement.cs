653146db5bbee44288532b0423a8d6d0779cd909
DracoMeteorStorm <adracometeorstorm@gmail.com>
2025-04-02 14:21:18 -0400

    enemy functions

--- Data Paws/Assets/Scripts/EnemyMovement.cs
+++ Data Paws/Assets/Scripts/EnemyMovement.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D myRigidBody;
+    public float runSpeed;
     void Start()
     {
-        
+        myRigidBody.linearVelocity = UnityEngine.Vector2.right*runSpeed;
     }
 
     // Update is called once per frame

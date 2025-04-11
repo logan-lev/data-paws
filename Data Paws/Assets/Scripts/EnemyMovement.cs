a075ef31847768cb425035b6ae0e8c170eba7aa2
DracoMeteorStorm <adracometeorstorm@gmail.com>
2025-04-11 14:20:08 -0400

    fixing enemy collisions

--- Data Paws/Assets/Scripts/EnemyMovement.cs
+++ Data Paws/Assets/Scripts/EnemyMovement.cs
@@ -10,7 +10,6 @@ public class EnemyMovement : MonoBehaviour
     public float runSpeed;
     void Start()
     {
-        runSpeed = 3;
         myRigidBody.linearVelocity = UnityEngine.Vector2.right*runSpeed;
         isMovingRight = true;
     }
@@ -31,17 +30,16 @@ public class EnemyMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if(myRigidBody.linearVelocityX == 0){
             if(isMovingRight){
-                myRigidBody.linearVelocity = UnityEngine.Vector2.left*runSpeed;
-                
+                myRigidBody.linearVelocity = UnityEngine.Vector2.left*runSpeed;        
             }
             else{
                 myRigidBody.linearVelocity = UnityEngine.Vector2.right*runSpeed;
-                
             }
             enemySprite.flipX = !enemySprite.flipX;
             isMovingRight = !isMovingRight;
             
-
+        }
     }
 }

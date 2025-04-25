775034d5eeef5bbc0fda802d82b6b4fb29392b81
DracoMeteorStorm <adracometeorstorm@gmail.com>
2025-04-25 14:42:17 -0400

    working on npcs

--- Data Paws/Assets/Scripts/NPCBehavior.cs
+++ Data Paws/Assets/Scripts/NPCBehavior.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCBehavior : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject npcDialogue;
+    public GameObject npcSample;
+    public Rigidbody2D npcBody;
+    public GameObject player;
+    public Rigidbody2D playerBody;
     void Start()
     {
-        
+        npcDialogue.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D npcBody)
+    {
+        npcDialogue.SetActive(true);
     }
 }

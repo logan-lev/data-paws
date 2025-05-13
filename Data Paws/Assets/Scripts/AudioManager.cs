ba342cc96b930669f064057ddec766d99dd6f1dc
Zhicheng Liu <zcliu1922@gmail.com>
2025-05-12 21:33:35 -0400

    Update for adding music volume slider functionality, and delete setting scene

--- Data Paws/Assets/Scripts/AudioManager.cs
+++ Data Paws/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     private static AudioManager instance;
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+
+    void Start()
+    {
+        if (musicSlider != null)
+        {
+            float value;
+            audioMixer.GetFloat("MusicVolume", out value);
+            musicSlider.value = Mathf.Pow(10, value / 20f); // convert dB to 0–1 scale
+        }
+    }
+
+    void Update()
+    {
+        if (musicSlider.value == 0)
+        {
+            audioMixer.SetFloat("MusicVolume", -80f); // Mute
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20); // Convert 0–1 scale to dB
+    }
+
 
     void Awake()
     {
